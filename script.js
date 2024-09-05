309efe9abdb10c1ce232f2a8713d4e17cf62f732
Mario Acendra <mariojoacre31@gmail.com>
2024-09-05 17:53:15 -0500

    Update

--- script.js
+++ script.js
@@ -1,11 +1,27 @@
 let hello1 = document.getElementById('hola1');
 let hello2= document.getElementById('hola2');
+let div=document.getElementById('button')
+let color = false;
 
-function changeColor()
+let newHeader= document.createElement('h1');
+div.appendChild(newHeader);
+newHeader.textContent='Presiona para cambiar Color del texto';
+newHeader.onclick=changeColor
 
 hello1.innerHTML="Adios"
 hello2.style.color='orange'
 
-let newHeader= document.createElement('div');
-newHeader.innerHTML="<Button onClick='changeColor()>Cambiar Color</button>'"
+
+function changeColor(){
+    if(color==false){
+        newHeader.style.color="red"
+        color=true
+    }else{
+        newHeader.style.color="black"
+        color=false
+    }
+}
+
+
+
 
